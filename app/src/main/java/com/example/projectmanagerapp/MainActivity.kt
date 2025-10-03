4caa26f73cdbcbd911f2660cdc3408052cfb6a5f
linyueCS-IT <6197673@johnabbottcollege.net>
2025-10-02 22:56:11 -0400

    Add project Wireframe
    1. Define app structure and layout
    2. Define all Screens and Responsibilities

--- app/src/main/java/com/example/projectmanagerapp/MainActivity.kt
+++ app/src/main/java/com/example/projectmanagerapp/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.projectmanagerapp.routes.Router
 import com.example.projectmanagerapp.ui.theme.ProjectManagerAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +15,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ProjectManagerAppTheme {
-
+                Router()
             }
         }
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun RouterPreview(){
+    ProjectManagerAppTheme {
+        Router()
+    }
+}
---
+++ app/src/main/java/com/example/projectmanagerapp/routes/Router.kt
@@ -0,0 +1,32 @@
+package com.example.projectmanagerapp.routes
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+// global variable for navigation
+val LocalNavController = compositionLocalOf<NavHostController> {
+    error("No NavController provided")
+}
+
+@Composable
+fun Router(){
+    val navController = rememberNavController()
+    CompositionLocalProvider(
+        LocalNavController provides navController,
+    ) {
+        NavHost(
+            navController = navController,
+            startDestination = "",
+        ){
+            composable ("") { }
+            composable ("") { }
+            composable ("") { }
+            composable ("") { }
+        }
+    }
+}
