4ae4843f97b8d74bd4b8d9e608a40055fdb53b1e
linyueCS-IT <6197673@johnabbottcollege.net>
2025-10-05 16:28:35 -0400

    feat: add information in project list if there is not project

--- app/src/main/java/com/example/projectmanagerapp/ui/LibraryScreen.kt
+++ app/src/main/java/com/example/projectmanagerapp/ui/LibraryScreen.kt
@@ -12,13 +12,16 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.projectmanagerapp.routes.LocalNavController
@@ -34,37 +37,56 @@ fun ProjectLibrary ( projectViewModel: ProjectViewModel = viewModel()){
     MainLayout(
         screenTitle = "Project Library"
     ) {
-        LazyColumn {
-            items(projects){ project ->
-                Card (
-                    modifier = Modifier
-                        .padding(10.dp)
-                        .fillMaxWidth(),
-                    elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
-                ){
-                    Column {
-                        Text( project.title)
+        if (projects.isEmpty()){
+            Card (
+                modifier = Modifier.padding(15.dp)
+                    .fillMaxWidth(),
+                elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+            ){
+                Column {
+                    Icon(
+                        imageVector = Icons.Default.Info,
+                        contentDescription = null,
+                    )
+                    Spacer(modifier = Modifier.padding(6.dp))
+                    Text("There are no projects at the moment.")
+                    Spacer(modifier = Modifier.padding(6.dp))
+                    Text("Please click the + button below to add one.")
+                }
+            }
+        } else {
+            LazyColumn {
+                items(projects){ project ->
+                    Card (
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .fillMaxWidth(),
+                        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+                    ){
+                        Column {
+                            Text( project.title)
 
-                        Spacer(modifier = Modifier.padding(10.dp))
+                            Spacer(modifier = Modifier.padding(10.dp))
 
-                        Text(project.description)
-                    }
-
-                    Row (
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.End
-                    )  {
-
-                        IconButton(onClick = {
-                            navController.navigate("DetailRoute/${project.projectId}")}
-                        ) {
-                            Icon(Icons.Default.Edit, contentDescription = "Edit")
+                            Text(project.description)
                         }
 
-                        IconButton(onClick = {
-                            projectViewModel.removeProject(project)}
-                        ) {
-                            Icon(Icons.Default.Delete, contentDescription = "Delete")
+                        Row (
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.End
+                        )  {
+
+                            IconButton(onClick = {
+                                navController.navigate("DetailRoute/${project.projectId}")}
+                            ) {
+                                Icon(Icons.Default.Edit, contentDescription = "Edit")
+                            }
+
+                            IconButton(onClick = {
+                                projectViewModel.removeProject(project)}
+                            ) {
+                                Icon(Icons.Default.Delete, contentDescription = "Delete")
+                            }
                         }
                     }
                 }
